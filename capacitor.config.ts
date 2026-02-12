e253f1bfec419be8215209c692efd70ee9cedf25
Crypto1181 <crypto1181@users.noreply.github.com>
2026-02-12 14:36:37 +0000

    Chore: jump version to 1.2.0 and build to 20 to fix binary conflict

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'Parachoot Soccer',
   webDir: 'dist',
   ios: {
-    buildNumber: '11'
+    buildNumber: '20'
   },
   server: {
     androidScheme: 'https'
